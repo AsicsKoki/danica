6002e8f9f6659cf37c3003724f5b61e6baa597cd
DanicaCarica <danicaozb@gmail.com>
2017-06-03 13:37:52 +0200

    Izlistavanje svih predmeta sa opisom

--- study.php
+++ study.php
@@ -1,3 +1,26 @@
+<?php 
+ 	session_start();
+
+	$connection = mysql_connect("localhost", "root", "", "project");
+	if(!$connection){
+		die("Could not connect:" . mysql_error());
+	}
+
+	mysql_select_db("project");
+
+ 	//check if the user is logggggged in
+	if(!isset($_SESSION['username'])){
+		header("Location: /danica/index.php");
+	}
+	$uid = $_SESSION["id"];
+
+ 	$sqlDefinitions     = "SELECT definitions.*, subject_name FROM definitions INNER JOIN subjects ON definitions.subject_id = subjects.id WHERE user_id = '$uid'";
+ 	$definitions       		= mysql_query($sqlDefinitions, $connection) or die(mysql_error());
+
+ 	$sqlSubjects = "SELECT * FROM subjects";
+ 	$subjects = mysql_query($sqlSubjects, $connection) or die(mysql_error());
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -29,6 +52,22 @@
 				    </ul>
 				  </div>
 			</nav>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-lg-12 col-sm-12 col-xs-12 content"><br/>
+					<ul>
+					<?php
+
+					 	while($subject = mysql_fetch_assoc($subjects)) {
+							?>
+							<li><a href="#"><b><?php echo $subject['subject_name']; ?></b></a> <br/><?php echo $subject['subject_description']; ?><?php  ?><br/><br/></li>
+						<?php
+					 	}
+					?>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
